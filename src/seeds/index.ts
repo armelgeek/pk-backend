b34d5471998a0ee7738ccef16630b8e97ab3ef90
EdLahatra <herypaslie.dell@gmail.com>
2024-01-24 20:48:51 +0300

    feat: Seeder

--- src/seeds/index.ts
+++ src/seeds/index.ts
@@ -1,8 +1,29 @@
 import { Connection } from 'typeorm';
 import { Factory, Seeder } from 'typeorm-seeding';
 import * as bcrypt from 'bcryptjs';
+import { UtilisateurRepository } from '../repository/Utilisateur';
 
 export class InitSeeds implements Seeder {
   // eslint-disable-next-line class-methods-use-this
-  async run(factory: Factory, connection: Connection): Promise<void> {}
+  async run(factory: Factory, connection: Connection): Promise<void> {
+    const user = connection.getCustomRepository(UtilisateurRepository);
+    const user_count = await user.find({ email: "fullstackadmin@gmail.com" });
+    if (!(user_count.length > 0)) {
+      await user.save([
+        {
+          password: await bcrypt.hashSync('PockerApp#123', 10),
+          nom: "PockerApp",
+          prenom: "Test",
+          telephone: "",
+          email: "pockerapps@gmail.com",
+          ville: "",
+          adresse: "",
+          // dateInscription: new Date(),
+          imageUrl: "",
+          role: 1,
+          actif: true,
+        },
+      ]);
+    }
+  }
 }

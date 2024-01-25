04d93cb8215e072ff39e2117adad917b6c684566
Pascal <dev.fullstack@gmail.com>
2024-01-25 09:34:03 +0300

    fix: ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,4 @@
 module.exports = {
-  // type: "mongodb",
-  // url: "mongo",
-  // port: "27017",
-  // database: "docker-node-mongo",
-  // password: "node1234",
-  // username: "root",
-  // driver: { useUnifiedTopology: true },
-  // logging: true,
-  // synchronize: true,
   "type": "mongodb",
   "host": "mongo",
   "port": 27017,
@@ -19,9 +10,10 @@ module.exports = {
   "synchronize": false,
   "entities": [
     "dist/data/do/**/*.js",
-    "src/data/do/**/*.ts"
+    // "src/data/do/**/*.ts"
   ],
   "seeds": [
-    "dist/seeds/*.js"
+    "dist/seeds/*.js",
+    // "src/seeds/*.ts"
   ],
 }

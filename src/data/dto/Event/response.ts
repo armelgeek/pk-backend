39e01b7067e4857efdca18d12eeb0b0094102c04
EdLahatra <herypaslie.dell@gmail.com>
2024-02-29 11:32:25 +0300

    fix: event

--- src/data/dto/Event/response.ts
+++ src/data/dto/Event/response.ts
@@ -15,6 +15,43 @@ export type EventResponseDTO = {
    private?: boolean;
    userList?: string[];
    profile?: string;
+   nbDay?: number;
+   nbGames?: number;
+   startStack?: string;
+   nbRebu?: number;
+   devise?: string;
+   latReg?: string;
+   dealer?: string;
+   timeClock?: string;
+   reEntry?: string;
+   memberCard?: string;
+   prizePool?: string;
+   bounty?: string;
+   bountyText?: string;
+   othersReward?: string;
+   tournamentType?: string;
+   gameType?: string;
+   jackpot?: string;
+   jackpotText?: string;
+   buyin?: string;
+   bonus?: string;
+   bonusText?: string;
+   addon?: string;
+   addonPrice?: string;
+   nbParticipantMax?: string;
+   massage?: string;
+   hotel?: boolean;
+   qualification?: string;
+   titleDay?: string;
+   startDateDay?: string;
+   startTimeDay?: string;
+   lateReg?: string;
+   limitRegEntry?: string;
+   durationDay?: number;
+   levels?: string[];
+   duration?: string;
+   type?: string;
+   limitRegBuy?: string;
   createdAt: Date;
   updatedAt: Date;
   id: string;
@@ -38,6 +75,43 @@ export type EventResponseDTO = {
  * @property {boolean} private
  * @property {array<string>} userList
  * @property {string} profile
+ * @property {number} nbDay
+ * @property {number} nbGames
+ * @property {string} startStack
+ * @property {number} nbRebu
+ * @property {string} devise
+ * @property {string} latReg
+ * @property {string} dealer
+ * @property {string} timeClock
+ * @property {string} reEntry
+ * @property {string} memberCard
+ * @property {string} prizePool
+ * @property {string} bounty
+ * @property {string} bountyText
+ * @property {string} othersReward
+ * @property {string} tournamentType
+ * @property {string} gameType
+ * @property {string} jackpot
+ * @property {string} jackpotText
+ * @property {string} buyin
+ * @property {string} bonus
+ * @property {string} bonusText
+ * @property {string} addon
+ * @property {string} addonPrice
+ * @property {string} nbParticipantMax
+ * @property {string} massage
+ * @property {boolean} hotel
+ * @property {string} qualification
+ * @property {string} titleDay
+ * @property {string} startDateDay
+ * @property {string} startTimeDay
+ * @property {string} lateReg
+ * @property {string} limitRegEntry
+ * @property {number} durationDay
+ * @property {array<string>} levels
+ * @property {string} duration
+ * @property {string} type
+ * @property {string} limitRegBuy
  * @property {string} id
  * @property {string} updatedAt
  * @property {string} createdAt

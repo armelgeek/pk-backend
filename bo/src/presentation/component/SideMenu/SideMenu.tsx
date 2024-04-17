63d940ae6279f3f47cf0e0f62415c78dfbd56364
EdLahatra <herypaslie.dell@gmail.com>
2024-04-17 11:29:01 +0300

    fix: code

--- bo/src/presentation/component/SideMenu/SideMenu.tsx
+++ bo/src/presentation/component/SideMenu/SideMenu.tsx
@@ -18,7 +18,6 @@ import inscription from '../../../assets/img/icon_BO/inscription.svg';
 import lot from '../../../assets/img/icon_BO/lot.svg';
 import parametre from '../../../assets/img/icon_BO/parametre.svg';
 import profil from '../../../assets/img/icon_BO/profil.svg';
-import { Sidebar } from '../Sidebar';
 import './Header.css';
 import logo from '../../../assets/img/logo.png';
 
@@ -63,7 +62,7 @@ export const SideMenu = ({ role }) => {
         }
       >
         <div className="menu-item mt-2">
-          <button className="flex justify-start items-center space-x-6  text-gray-400 rounded px-3 py-2  w-full md:w-52" onClick={() => na}>
+          <button className="flex justify-start items-center space-x-6  text-gray-400 rounded px-3 py-2  w-full md:w-52">
             <svg className="fill-stroke" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M12 11C14.2091 11 16 9.20914 16 7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7C8 9.20914 9.79086 11 12 11Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
               <path d="M6 21V19C6 17.9391 6.42143 16.9217 7.17157 16.1716C7.92172 15.4214 8.93913 15 10 15H14C15.0609 15 16.0783 15.4214 16.8284 16.1716C17.5786 16.9217 18 17.9391 18 19V21" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>

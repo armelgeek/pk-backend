5259b200f7b405b946542b6b09f68e69997291ea
EdLahatra <herypaslie.dell@gmail.com>
2024-05-02 08:49:31 +0300

    fix: partner

--- src/data/dto/Partener/response.ts
+++ src/data/dto/Partener/response.ts
@@ -2,6 +2,20 @@ export type PartenerResponseDTO = {
    name?: string;
    logo?: string;
    description?: string;
+   phone?: PhoneRequestDTO;
+   verification?: string;
+   socialNetwork?: string[];
+   profileId?: string;
+   price?: number;
+   autoRenewed?: boolean;
+   subscriptionStripeId?: string;
+   title?: string;
+   hendonmob?: number;
+   rangAllTimeMoney?: string;
+   bracelet?: number;
+   job?: string;
+   type?: string;
+   message?: string;
   createdAt: Date;
   updatedAt: Date;
   id: string;
@@ -12,6 +26,20 @@ export type PartenerResponseDTO = {
  * @property {string} name
  * @property {string} logo
  * @property {string} description
+ * @property {PhoneRequestDTO} phone
+ * @property {string} verification
+ * @property {array<string>} socialNetwork
+ * @property {string} profileId
+ * @property {number} price
+ * @property {boolean} autoRenewed
+ * @property {string} subscriptionStripeId
+ * @property {string} title
+ * @property {number} hendonmob
+ * @property {string} rangAllTimeMoney
+ * @property {number} bracelet
+ * @property {string} job
+ * @property {string} type
+ * @property {string} message
  * @property {string} id
  * @property {string} updatedAt
  * @property {string} createdAt

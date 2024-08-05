0f8e4dec65236a4e2bc94076bb82a5a60be2ec76
Pascal <dev.fullstack@gmail.com>
2024-08-05 16:16:14 +0300

    fix: code

--- src/service/applicatif/SharingNotes.ts
+++ src/service/applicatif/SharingNotes.ts
@@ -6,9 +6,9 @@ import {
 import { SharingNoteDO } from '../../data/do/SharingNote';
 
 // @ts-ignore
-import { SharingNoteRequestDTO } from '../../data/dto/SharingNote/request';
+import { SharingNoteRequestDTO } from '../../data/dto/SharingNotes/request';
 // @ts-ignore
-import { SharingNoteResponseDTO } from '../../data/dto/SharingNote/response';
+import { SharingNoteResponseDTO } from '../../data/dto/SharingNotes/response';
 import { sharingnoteSM, SharingNoteSM } from '../metier/SharingNote';
 
 export class SharingNoteSA extends GenericSA<
